c03ebda435fe10174ff52a178ac474ac237c6e73
Igor Cuckovic <cuckovic@gmail.com>
2016-01-23 13:23:41 +0100

    added completed filter

--- server.js
+++ server.js
@@ -16,7 +16,20 @@ app.get("/", function (req, res) {
 // GET /todos
 // GET /todos/1 // :id
 app.get("/todos", function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	// if has property && completed === 'true'
+	// 		filteredTodos = _.where(filteredTodos, ?)
+	// else if has prop && completed if false
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}	
+
+	res.json(filteredTodos);
 });
 
 app.get("/todos/:id", function (req, res) {
